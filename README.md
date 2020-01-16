105981faff5bc1bfb4a3b5c18d068bbf18c50fef
Roman Charugin <rcharugin@degiro.nl>
2020-01-16 19:14:27 +0200

    WIP: Starting work on install.sh improovements
    - Add software installation for mac
    - Configuring git
    - Connecting with a github
    - Removed unused config: tmux, screen, sublime

---
+++ README.md
@@ -0,0 +1,11 @@
+# dotfiles
+
+## Installation
+
+### MacOS
+
+```sh
+curl -s https://raw.githubusercontent.com/RomiC/dotfiles/master/install.sh | bash -s
+```
+
+### Windows (PowerShell)
--- install.sh
+++ install.sh
@@ -2,8 +2,43 @@
 
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
+echo "\n=[ Installing Software ]=\n"
+
+echo "-> brew\n"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew update
+
+echo "-> git\n"
+brew install git
+
+echo "-> zsh\n"
+brew install zsh
+
+echo "-> vim\n"
+brew install vim
+
+echo "-> iTerm2\n"
+brew cask install iterm2
+
+echo "-> Visual Studio Code\n"
+brew cask install visual-studio-code
+
+echo "-> Telegram\n"
+brew cask install telegram
+
+echo "\n=[ Configuring Git ]=\n"
+echo -n "\n> Enter name: " read GIT_NAME
+echo -n "\n> Enter email: " read GIT_EMAIL
+ssh-keygen -t RSA -C $GIT_EMAIL -N '' -f $HOME/.ssh/id_rsa
+
+echo "\n=[ Connecting to GitHub ]=\n"
+echo -n "\n> Enter GitHub personal token: " read -s GITHUB_TOKEN
+curl -v -H "Authorization: token $GITHUB_TOKEN" --data '{"title":"'$GIT_EMAIL'","key":"'"$(cat $HOME/.ssh/id_rsa.pub)"'"}' https://api.github.com/user/keys
+
+git config --global user.name $GIT_NAME
+git config --global user.email $GIT_EMAIL
+
 ln -sf $DOTFILES_DIR/.gitattributes $HOME/.gitattributes
-ln -sf $DOTFILES_DIR/.gitconfig $HOME/.gitconfig
 ln -sf $DOTFILES_DIR/.vim $HOME/.vim
 ln -sf $DOTFILES_DIR/.vimrc $HOME/.vimrc
 ln -sf $DOTFILES_DIR/.zshrc $HOME/.zshrc

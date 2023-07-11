9fe7c9d4beca67b3a8f6b67e71ca1024d217b34e
Roman <roman.charugin@acronis.com>
2023-07-11 18:29:38 +0300

    Updates:
    - Made acronis_npm_up script more bulletproof
    - Set default git branch to "main"

--- install.sh
+++ install.sh
@@ -127,6 +127,7 @@ git config --global user.name $GIT_NAME
 git config --global user.email $GIT_EMAIL
 git config --global core.excludesfile $DOTFILES_DIR/.gitignore_global
 git config --global core.editor "nvim"
+git config --global init.defaultBranch main
 ln -sf $DOTFILES_DIR/.gitattributes $HOME/.gitattributes
 
 mkdir -p $HOME/.config

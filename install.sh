e0b9b68b90a034ea51ee46e6e989c65172eefc8c
Roman Charugin <romic+github@fastmail.com>
2026-05-09 08:06:01 +0300

    Remove im-select installation from install.sh
    Removed installation steps for im-select from the script.

--- install.sh
+++ install.sh
@@ -42,14 +42,6 @@ ln -sf $DOTFILES_DIR/.Brewfile $HOME/.Brewfile
 $BREW_BIN bundle --global
 $BREW_BIN cleanup
 
-echo '-> im-select'  # necessary for VSCode
-if [[ -e /usr/local/bin/im-select ]]; then
-    rm -f /usr/local/bin/im-select
-fi
-curl -Ls -o /usr/local/bin/im-select https://github.com/daipeihust/im-select/raw/master/macOS/out/intel/im-select
-chmod +x /usr/local/bin/im-select
-
-
 echo '=[ Configuring ]='
 
 mkdir -p $HOME/.config
